caf7fefc94fdad3737086290078bce5b67639ea2
KHovhannisyan <Lsargsyan@VTANGAVOR>
2018-09-27 15:20:02 +0400

    added

--- Homework 2/Homework 2/Program.cs
+++ Homework 2/Homework 2/Program.cs
@@ -10,30 +10,30 @@ namespace Homework_2
 	{
 		static void Main(string[] args)
 		{
+
 			Console.Write("Enter your Last Name ");
 			string lastname = Console.ReadLine();
 
 			Console.Write("Enter your First name ");
 			string firstname = Console.ReadLine();
 
-
-			Console.Write("Enter your Birth Year ");
-			int birtyear = Convert.ToInt16(Console.ReadLine());
-			int year = 2018 - birtyear;
-
 			Console.Write("Enter your Weight ");
 			byte weigh = Convert.ToByte(Console.ReadLine());
 
+			Console.Write("Enter your Birth Year ");
+			short birthyear = Convert.ToInt16(Console.ReadLine());
+			int year = 2018 - birthyear;
+
 			if (weigh < 99.9)
 			{
-				Console.WriteLine("Dear  {lastname} {firstname} you are {year} year old and thin man");
+				Console.WriteLine($"Dear  {lastname} {firstname} you are {year} year old and thin man");
 			}
 			else
 			{
-				Console.WriteLine("Dear  {lastname} {firstname} you are {year} year old and fat man");
-
-				
+				Console.WriteLine($"Dear  {lastname} {firstname} you are {year} year old and fat man");
 			}
+
+
 			Console.ReadKey();
 
 		}
